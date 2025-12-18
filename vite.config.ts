62f580a1bafd28fbb4cb2f0a1289fe42d949f9d0
meisammhz <meisammahmodzade@gmail.com>
2025-12-18 10:17:42 +0330

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,38 @@
 // without relying on global Node types which were causing compilation errors.
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
-    plugins: [react()],
+    plugins: [react(), VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,webmanifest}'],
+        maximumFileSizeToCacheInBytes: 5 * 1024 * 1024 // 5 MB
+      },
+      manifest: {
+        name: 'سامانه مدیریت پروژه نارنجستان ۷',
+        short_name: 'نارنجستان ۷',
+        description: 'سامانه مدیریت پروژه نارنجستان ۷',
+        theme_color: '#22c55e',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })],
     define: {
       // Vite replaces `process.env.API_KEY` with the value of `env.API_KEY` during build
       'process.env.API_KEY': JSON.stringify(env.API_KEY)
